6602c86e5ec3762080045b95d5d24ed89b79aa0c
edgyboii xx <cykablyat7834@gmail.com>
2025-10-04 14:39:47 +0300

    + 9 (1,2,3)

---
+++ 02.02/ЛР9/LabWork9/Task1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        string filePath = "data.txt";
+       
+        Console.ForegroundColor = ConsoleColor.Green;
+        await Profiler.ProfileReading(filePath);
+    }
+}
+
+public class Project
+{ 
+    public void ReadFromFile(string filePath)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("File not found.");
+            return;
+        }
+
+        using (var reader = new StreamReader(filePath))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+
+    public async Task ReadFromFileAsync(string filePath, int bufferSize = 8192)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("File not found.");
+            return;
+        }
+
+        using var fileStream = new FileStream(
+            filePath,
+            FileMode.Open,
+            FileAccess.Read,
+            FileShare.Read,
+            bufferSize,
+            useAsync: true);
+
+        using var reader = new StreamReader(fileStream, bufferSize: bufferSize);
+
+        string line;
+        while ((line = await reader.ReadLineAsync()) != null)
+        {
+            Console.WriteLine(line);
+        }
+          
+    }
+}
+
+public static class Profiler
+{
+    public static async Task ProfileReading(string filePath)
+    {
+        var project = new Project();
+
+        //синхронный метод
+        var stopwatch = Stopwatch.StartNew();
+        project.ReadFromFile(filePath);
+        stopwatch.Stop();
+
+        long syncMs = stopwatch.ElapsedMilliseconds;
+
+        //асинхронныый метод
+        stopwatch.Restart();
+
+        await project.ReadFromFileAsync(filePath, bufferSize: 8192);
+        stopwatch.Stop();
+        long asyncMs = stopwatch.ElapsedMilliseconds;
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
+
+        Console.WriteLine("\n=== РЭЗУЙЛЬТАУТЫ ===");
+        Console.WriteLine($"Синхроунный метод:   {syncMs} мс");
+        Console.WriteLine($"Асинхроунный метод:  {asyncMs} мс");
+        Console.WriteLine($"Раузница:            {syncMs - asyncMs} мс");
+        Console.ResetColor();
+    }
+}
---
+++ 02.02/ЛР9/LabWork9/Task3/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var project = new Project();
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("Запуск профилирования вычисления Фибоначчи...\n");
+
+        await Profiler.ProfileFibonacci(project, 40);
+    }
+}
+
+public class Project
+{
+    private readonly Dictionary<int, int> _fibonacciCache = new Dictionary<int, int>();
+
+    public int Fibonacci(int n)
+    {
+        if (n <= 1)
+            return n;
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+
+    public int FibonacciFast(int n)
+    {
+        if (n <= 1)
+            return n;
+
+        if (_fibonacciCache.TryGetValue(n, out int cachedValue))
+            return cachedValue;
+        
+
+        int result = FibonacciFast(n - 1) + FibonacciFast(n - 2);
+
+        _fibonacciCache[n] = result;
+
+        return result;
+    }
+}
+
+public static class Profiler
+{
+    public static async Task ProfileFibonacci(Project project, int count)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Вычисление (без кэша):");
+        var stopwatch = Stopwatch.StartNew();
+        for (int i = 0; i < count; i++)
+        {
+            int value = project.Fibonacci(i);
+            Console.WriteLine($"Fib({i}) = {value}");
+        }
+        stopwatch.Stop();
+        long naiveMs = stopwatch.ElapsedMilliseconds;
+
+        Console.WriteLine("\n" + new string('=', 40) + "\n");
+
+        project = new Project();
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Вычисление (с кэшированием):");
+        stopwatch.Restart();
+        for (int i = 0; i < count; i++)
+        {
+            int value = project.FibonacciFast(i);
+            Console.WriteLine($"Fib({i}) = {value}");
+        }
+        stopwatch.Stop();
+        long optimizedMs = stopwatch.ElapsedMilliseconds;
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("\n=== Результаты профилирования вычислений ===");
+        Console.WriteLine($"Без кэширования:    {naiveMs} мс");
+        Console.WriteLine($"С кэшированием:     {optimizedMs} мс");
+        Console.WriteLine($"Ускорение:          {naiveMs - optimizedMs} мс");
+        Console.ResetColor();
+    }
+}
